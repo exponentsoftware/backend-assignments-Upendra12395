798ee9b7b2f506209eac0543c6d1504d5a8700df
Upendra12395 <ch95upendra@gmail.com>
2021-10-26 15:54:06 +0530

    user routes

---
+++ day3/routes/user.js
@@ -0,0 +1,10 @@
+const express =  require('express')
+const router = express.Router()
+const userController = require('../controllers/user')
+
+router.get('/',userController.getAll)
+router.post('/addUser', userController.addUser)
+router.get('/:id', userController.getOneUser)
+router.patch('/:id', userController.updateOneUser)
+router.delete('/', userController.deleteAllUser)
+router.delete('/:id', userController.deleteOneUser)
